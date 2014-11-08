501d571a546405a6556205e9127eaec1277cc34f
Joe Bordes <joe@tsolucio.com>
2014-11-08 22:40:17 +0100

    pickup base code from old 5.2 version

---
+++ modules/cbEmployee/language/es_es.lang.php
@@ -0,0 +1,52 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Employees' => 'Empleados',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_EMPLOYEES_INFORMATION' => 'Información de Empleado',
+	'LBL_ECONOMIC_INFORMATION' => 'Información Laboral',
+	'LBL_DESCRIPTION_INFORMATION' => 'Información Adicional',
+
+	'matricula' => 'Matrícula',
+	'nss' => 'NSS',
+	'nif' => 'NIF',
+	'birthplace' => 'Lugar de Nacimiento',
+	'birthdate' => 'Fecha de Nacimiento',
+	'age' => 'Edad',
+	'maritalstatus' => 'Estado Civil',
+	'children' => 'Nº de Hijos',
+	'inchargeof' => 'Personas a su cargo',
+	'userid' => 'Usuario',
+	'situacion' => 'Situación',
+	'categoria' => 'Categoría',
+	'tipocontrato' => 'Tipo de Contrato',
+	'nombramiento' => 'Nombramiento',
+	'departamento' => 'Departamento',
+	'tomaposesion' => 'Toma de Posesión',
+	'altaempresa' => 'Alta en la Empresa',
+	'altapuesto' => 'Alta en el Puesto',
+	'description' => 'Descripción',
+	
+	'LBL_POINTS_INFORMATION' => 'Información de Puntos',
+	'pts_adquiridos' => 'Total de puntos adquiridos',
+	'pts_consumidos' => 'Total de puntos consumidos',
+	'pts_saldo' => 'Saldo final de puntos',
+
+	'No tiene' => 'No tiene',
+	'Hijos' => 'Hijos',
+	'Padres' => 'Padres',
+	'Abuelos' => 'Abuelos',
+	'Conyuge' => 'Cónyuge',
+	'Otros' => 'Otros',
+);
+
+?>
---
+++ modules/cbEmployee/language/it_it.lang.php
@@ -0,0 +1,46 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'ModuleName' => 'Module Name',
+
+	'LBL_CUSTOM_INFORMATION' => 'Informazioni Personalizzate',
+	'LBL_MODULEBLOCK_INFORMATION' => 'ModuleBlock Information',
+        'Employees' => 'Medici',
+        'SINGLE_Employees' => 'Medico',
+              
+        'LBL_EMPLOYEES_INFORMATION' => 'Informazioni Medico',
+        'matricula' => 'Matricola',
+        'birthplace' => 'Luogo di Nascita',
+        'birthdate' => 'Data di Nascita',
+        'maritalstatus' => 'Stato Civile',
+        'children' => 'Prole',
+        'inchargeof' => 'Incarico',
+        'userid' => 'ID Utente',
+        
+        'LBL_ECONOMIC_INFORMATION' => 'Informazioni Economiche',
+        'situacion' => 'Situazione',
+        'categoria' => 'Categoria',
+        'tipocontrato' => 'Tipo di Contratto',
+        'nombramiento' => 'Appuntamento',
+        'departamento' => 'Diparimento',
+        'tomaposesion' => 'Acquisito',
+        'altaempresa' => 'Inizio Accordo',
+        'altapuesto' => 'Inizio Collaborazione',
+        
+        
+        'LBL_DESCRIPTION_INFORMATION' => 'Informazioni Descrizione',
+        'description' => 'Descrizione',
+        
+        
+	'ModuleFieldLabel' => 'ModuleFieldLabel Text',
+);
+
+?>
